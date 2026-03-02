7a1250cc11dcd95a0c36e1090d37fdc42745bfef
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 01:14:54 +0530

    Add StringPalindrome_BuildReverse class

---
+++ Strings/StringPalindrome_BuildReverse.java
@@ -0,0 +1,16 @@
+public class StringPalindrome_BuildReverse {
+
+    public static boolean isPalindrome(String str) {
+
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--)
+            rev += str.charAt(i);
+
+        return str.equals(rev);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+    }
+}
