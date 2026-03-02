11d0c4aa28b92184e1fd62a9f4970a506bc1f155
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 00:31:43 +0530

    Add ReverseArray class to reverse an integer array
    Implement a program to reverse an array in place.

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,18 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10};
+
+        int start = 0, end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
