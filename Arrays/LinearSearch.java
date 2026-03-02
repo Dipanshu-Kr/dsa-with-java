fab61283a7309f0f0871fba0e9f49e8ba66faee8
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 00:30:22 +0530

    Implement Linear Search in LinearSearch.java

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16};
+        int key = 10;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Found at index " + i);
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
