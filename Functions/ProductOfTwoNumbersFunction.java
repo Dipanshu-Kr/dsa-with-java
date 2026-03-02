ec401fb9b3f62e0b624acb82d544fcc7fdb900e4
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 00:19:59 +0530

    Implement product function for two numbers

---
+++ Functions/ProductOfTwoNumbersFunction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ProductOfTwoNumbersFunction {
+
+    public static int productOfNum(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(productOfNum(a, b));
+    }
+}
