7a47dd51a7d5c3822a7e18289d703c6047b5cf3c
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-04 14:36:26 +0530

    Add ClearRangeOfBits class with bit manipulation method
    Implement method to clear bits in a specified range.

---
+++ Bit-Manipulation/ClearRangeOfBits.java
@@ -0,0 +1,17 @@
+public class ClearRangeOfBits {
+
+    public static int clearRangeOfBits(int n, int i, int j) {
+
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - 1;
+
+        int bitmask = a | b;
+
+        return n & bitmask;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(clearRangeOfBits(n, 2, 4));
+    }
+}
