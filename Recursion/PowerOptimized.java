584b71090f85d7267067d6af4fbf8aabe3a2d06b
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-25 19:55:48 +0530

    Add optimized power calculation using recursion

---
+++ Recursion/PowerOptimized.java
@@ -0,0 +1,12 @@
+public class PowerOptimized {
+    public static int power(int x, int n) {
+        if (n == 0) return 1;
+        int half = power(x, n / 2);
+        if (n % 2 == 0) return half * half;
+        return x * half * half;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 11));
+    }
+}
